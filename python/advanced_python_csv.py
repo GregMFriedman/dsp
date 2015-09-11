51e7c96d5dd0fc85a7cefd9ee6b393714df028d9
GREGORY FRIEDMAN <greg.m.friedman@gmail.com>
2015-09-11 13:29:52 -0400

    solve

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,13 @@
-PLACE YOUR CODE HERE
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 11 10:12:49 2015
+
+@author: gregoryfriedman
+"""
+
+import pandas as pd
+#import numpy as np
+
+fd = pd.read_csv('faculty.csv')
+fd[' email'].to_csv('emails.csv', index = False)
+print pd.read_csv('emails.csv')
